6558d5583bbdce03b58d3378dbb5acfbba7c18de
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2019-09-27 13:19:12 -0600

    'done'

--- src/main.js
+++ src/main.js
@@ -2,15 +2,28 @@
 const apiKey = 'qTIaiaU3kkW2zggcusNtaSrGPOiDsMhKF0INjbTH';
 const url = 'https://developer.nps.gov/api/v1/parks';
 
+let results = document.getElementById('results');
 
+function displayPark(data) {
+    console.log(data);
+    data.forEach(item => results.innerHTML += `<div> 
+<span><h2>${item.fullName}</h2> </span>
+<span><h3>${item.description}</h3> </span>
+<span><h3>${item.url}</h3> </span>
+ </div>`)
+}
 function getParks() {
 
+    const parks = [];
     const limit = document.getElementById('js-max-results').value;
     const stateCodes = document.getElementById('js-search-term').value;
+    results.innerHTML = '';
 
     fetch(`${url}?api_key=${apiKey}${(stateCodes) ? '&stateCode='+stateCodes : ''}${(limit > 0 && limit <= 50) ? '&limit=' + limit : '&limit=10'}`)
         .then(response => response.json())
-        .then(responseJson => console.log(responseJson));
+        .then(responseJson => responseJson.data.forEach(post => parks.push(post)))
+        .then(responseJson => console.log(responseJson))
+        .then(() => displayPark(parks));
 }
 
 
